b71787813e6acbcf527c3d7ae516f45c3e8f61b9
miroshinsergey <msv@forward.lc>
2020-08-20 17:58:25 +0300

    resolve conflict

--- internal/event-service/model.go
+++ internal/event-service/model.go
@@ -1,9 +1,20 @@
 package event_service
 
-import "github.com/jinzhu/gorm"
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/jinzhu/gorm"
+	"strings"
+)
 
 const (
-	monday base = "1"
+	monday    base = "1"
+	thuesday  base = "2"
+	wednesday base = "3"
+	thursday  base = "4"
+	friday    base = "5"
+	saturday  base = "6"
+	sunday    base = "7"
 )
 
 type base string
@@ -17,19 +28,25 @@ type EventsType struct {
 
 type Events struct {
 	gorm.Model
-	Type     EventsType `gorm:"ForeignKey:TypeId;AssociationForeignKey:id"`
-	TypeId   int        `json:"type_id"`
-	Name     string     `json:"name"`
-	Days     string     `json:"days"`
-	IsActive bool       `json:"is_active"`
+	Type        EventsType `gorm:"ForeignKey:TypeId;AssociationForeignKey:id"`
+	TypeId      int        `json:"type_id"`
+	Name        string     `json:"name"`
+	Days        string     `json:"days"`
+	IsActive    bool       `json:"is_active"`
+	Description string     `json:"description"`
+	Price       int        `json:"price"`
+	StartTime   string     `json:"start_time"`
 }
 
 func (d *Events) UnmarshalJSON(data []byte) error {
 	type income struct {
-		TypeId   int      `json:"type_id"`
-		Name     string   `json:"name"`
-		Days     []string `json:"days"`
-		IsActive bool     `json:"is_active"`
+		TypeId      int      `json:"type_id"`
+		Name        string   `json:"name"`
+		Days        []string `json:"days"`
+		IsActive    bool     `json:"is_active"`
+		Description string   `json:"description"`
+		Price       int      `json:"price"`
+		StartTime   string   `json:"start_time"`
 	}
 
 	var inc income
@@ -42,6 +59,9 @@ func (d *Events) UnmarshalJSON(data []byte) error {
 	d.TypeId = inc.TypeId
 	d.IsActive = inc.IsActive
 	d.Days = fmt.Sprintf("{%s}", strings.Join(inc.Days, ","))
+	d.StartTime = inc.StartTime
+	d.Price = inc.Price
+	d.Description = inc.Description
 
 	return nil
 }
@@ -49,21 +69,27 @@ func (d *Events) UnmarshalJSON(data []byte) error {
 func (d Events) MarshalJSON() ([]byte, error) {
 	type outcome struct {
 		gorm.Model
-		Type     EventsType `gorm:"ForeignKey:TypeId;AssociationForeignKey:id"`
-		Name     string     `json:"name"`
-		Days     []string   `json:"days"`
-		IsActive bool       `json:"is_active"`
+		Type        EventsType `gorm:"ForeignKey:TypeId;AssociationForeignKey:id"`
+		Name        string     `json:"name"`
+		Days        []string   `json:"days"`
+		IsActive    bool       `json:"is_active"`
+		Description string     `json:"description"`
+		Price       int        `json:"price"`
+		StartTime   string     `json:"start_time"`
 	}
 
 	var days = strings.Replace(d.Days, "{", "", 1)
 	days = strings.Replace(days, "}", "", 1)
 
 	var out = outcome{
-		Model:    d.Model,
-		Name:     d.Name,
-		IsActive: d.IsActive,
-		Type:     d.Type,
-		Days:     strings.Split(days, ","),
+		Model:       d.Model,
+		Name:        d.Name,
+		IsActive:    d.IsActive,
+		Type:        d.Type,
+		Days:        strings.Split(days, ","),
+		Description: d.Description,
+		Price:       d.Price,
+		StartTime:   d.StartTime,
 	}
 
 	return json.Marshal(out)
