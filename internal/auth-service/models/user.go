f49b4ed279afd7d58a1acbddcb837b62bdf82eef
miroshinsergey <msv@forward.lc>
2020-11-18 12:04:43 +0300

    remove auth use model fields

--- internal/auth-service/models/user.go
+++ internal/auth-service/models/user.go
@@ -1,10 +1,7 @@
 package models
 
 type User struct {
-	Email     *string `json:"email"`
-	Password  *string `json:"password"`
-	Role      *int    `json:"role"`
-	FirstName *string `json:"first_name"`
-	LastName  *string `json:"last_name"`
-	AvatarUrl *string `json:"avatar_url"`
+	Email    *string `json:"email"`
+	Password *string `json:"password"`
+	Role     *int    `json:"role"`
 }
