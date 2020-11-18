d7288d2ccbe3badf371f9d622941098685ab8495
miroshinsergey <msv@forward.lc>
2020-11-18 11:37:33 +0300

    add fileds to model

--- internal/auth-service/models/user.go
+++ internal/auth-service/models/user.go
@@ -1,7 +1,10 @@
 package models
 
 type User struct {
-	Email    *string `json:"email"`
-	Password *string `json:"password"`
-	Role     *int    `json:"role"`
+	Email     *string `json:"email"`
+	Password  *string `json:"password"`
+	Role      *int    `json:"role"`
+	FirstName *string `json:"first_name"`
+	LastName  *string `json:"last_name"`
+	AvatarUrl *string `json:"avatar_url"`
 }
