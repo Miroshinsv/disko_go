10637241ef97d982e9e6f7b821c2854599e1e08b
miroshinsergey <msv@forward.lc>
2021-02-11 18:54:35 +0300

    cors

--- internal/web-server/router.go
+++ internal/web-server/router.go
@@ -20,6 +20,8 @@ var WebRouter *mux.Router
 func jsonMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Credentials", "true")
+		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS")
 		w.Header().Add("Content-Type", "application/json")
 		next.ServeHTTP(w, r)
 	})
